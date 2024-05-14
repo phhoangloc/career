9b21e42543d1de42f9f78555d8ee995df6cf3ea8
phhoangloc <82487565+phhoangloc@users.noreply.github.com>
2024-05-14 18:51:53 +0900

    0514

--- src/app/home/search/[[...slug]]/page.tsx
+++ src/app/home/search/[[...slug]]/page.tsx
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from 'react'
 import Data from '@/data/data'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
+import SearchTool from '@/component/searchTool'
+import SearchIcon from '@mui/icons-material/Search';
+import Button from '@/component/input/button'
 type Props = {
     params: { slug: string[] }
 }
@@ -34,35 +37,47 @@ const Page = ({ params }: Props) => {
     }, [])
 
     const toPage = useRouter()
+
+    const [searchModal, setSearchModal] = useState<boolean>(false)
     return (
         <div className='searchPage'>
-            <div className='title'>
-                <h1>検索結果</h1>
-            </div>
-            {newData.length ?
-                newData.map((item, index) =>
-                    <div key={index} className='item flexbox' style={{ cursor: "pointer" }} onClick={() => toPage.push("/home/" + item.archive + "/" + item.slug)}>
-                        <div className='image' style={{
-                            position: "relative",
-                        }}>
-                            <Image src={item.image} fill
-                                style={{
-                                    objectFit: 'cover',
-                                }}
-                                alt="cover" />
-                        </div>
-                        <div className='content'>
-                            <h4>{item.name}・{item.location}</h4>
-                            <li>{item.workplaces}</li>
-                            <li>{item.worktypes}</li>
-                            <li>{item.location}</li>
-                            <p>{item.contain[0].sort}</p>
+            <div className={`searchPage_max ${searchModal ? "searchPage_max_open" : ""}`}>
+                <div className='search_tool_div'>
+                    <SearchTool />
+                </div>
+                <div className='result_div'>
+                    <div className='title'>
+                        <h1>検索結果</h1>
+                        <div style={{ width: "max-content", margin: "0px auto 10px" }}>
+                            <Button name='検索' onClick={() => setSearchModal(!searchModal)} />
                         </div>
                     </div>
-                ) :
-                <div>
-                    <h2 style={{ textAlign: "center" }}>結果がありません。</h2>
-                </div>}
+                    {newData.length ?
+                        newData.map((item, index) =>
+                            <div key={index} className='item flexbox' style={{ cursor: "pointer" }} onClick={() => toPage.push("/home/" + item.archive + "/" + item.slug)}>
+                                <div className='image' style={{
+                                    position: "relative",
+                                }}>
+                                    <Image src={item.image} fill
+                                        style={{
+                                            objectFit: 'cover',
+                                        }}
+                                        alt="cover" />
+                                </div>
+                                <div className='content'>
+                                    <h4>{item.name}・{item.location}</h4>
+                                    <li>{item.workplaces}</li>
+                                    <li>{item.worktypes}</li>
+                                    <li>{item.location}</li>
+                                    <p>{item.contain[0].sort}</p>
+                                </div>
+                            </div>
+                        ) :
+                        <div>
+                            <h2 style={{ textAlign: "center" }}>結果がありません。</h2>
+                        </div>}
+                </div>
+            </div>
         </div>
     )
 }
